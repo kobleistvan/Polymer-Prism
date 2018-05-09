b0872e8a67cd4a8be87d07f791d6cf25627a7a83
Koble Istvan <kobleistvan@gmail.com>
2018-05-09 14:41:33 +0300

    WIP

--- prism.js
+++ prism.js
@@ -1,16 +1,36 @@
 var panels = chrome && chrome.devtools && chrome.devtools.panels;
+var bkg = chrome.extension.getBackgroundPage();
 
 var getPropsForSidebar = function () {
+    console.log('???', $0);
     if ($0) {
-        var props = Object.assign({}, $0.__data__, $0.__data);
-        return props;
+        return Object.assign({}, $0.__data__, $0.__data);
     }
 };
 
+var getob = function () {
+    return {test: 2};
+}
+
 var createSidebarPaneCallback = function (sidebar) {
-    panels.elements.onSelectionChanged.addListener(function updateElementProperties() {
-        sidebar.setExpression("(" + getPropsForSidebar.toString() + ")()");
-    });
+    // sidebar.setPage("hello.html");
+
+    // Update element properties
+    function updateElementProperties() {
+        // sidebar.setExpression("(" + getPropsForSidebar + ")()");        
+        sidebar.setObject(getPropsForSidebar());        
+    }
+
+    // Immediately update data on selection change
+    panels.elements.onSelectionChanged.addListener(updateElementProperties);
+
+    // Update data periodically
+    setInterval(updateElementProperties, 1000);
 };
 
+panels.create("Polymer Prism", "icon128.png", "hello.html", function(panel) {
+    bkg.console.log('Hi', panel);
+});
 panels && panels.elements.createSidebarPane("Polymer Prism", createSidebarPaneCallback);
+bkg.console.log(panels);
+bkg.console.log(panels.elements);
